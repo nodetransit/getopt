4f1e7b94dfea5051724c134a023b71d3782dc8c7
theurbanwanderess <shasheecosca@gmail.com>
2021-09-23 03:40:17 +1000

    separate demos

---
+++ tst/demo1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <getopt.hpp>
+
+int
+main(int argc, const char **argv)
+{
+    auto show_help = [&]() {
+        std::cout << argv[0]
+                  << " [-h|--help|-?] [-f=path|--file=path] [-v|--version] [-d=number|--depth=number|--max-depth=number]"
+                  << std::endl;
+        exit(0);
+    };
+
+    // Simple functional api. No initialization required.
+
+    bool        help    = getarg(false, "-h", "--help", "-?");
+    int         version = getarg(0, "-v", "--version", "--show-version");
+    int         depth   = getarg(0, "-d", "--depth", "--max-depth");
+    std::string file    = getarg("", "-f", "--file");
+
+    if (help || argc <= 1) {
+        show_help();
+        return EXIT_SUCCESS;
+    }
+
+    if (version) {
+        std::cout << argv[0] << " demo v1.0.0. Compiled on " << __DATE__ << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    if (depth) {
+        std::cout << "provided depth: " << depth << std::endl;
+    }
+
+    if (!file.empty()) {
+        std::cout << "provided file: " << file << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
--- tst/demo2.cpp
+++ tst/demo2.cpp
@@ -12,39 +12,18 @@ main(int argc, const char **argv)
         exit(0);
     };
 
-    // Simple functional api. No initialization required.
-
-    bool        help    = getarg(false, "-h", "--help", "-?");
-    int         version = getarg(0, "-v", "--version", "--show-version");
-    int         depth   = getarg(0, "-d", "--depth", "--max-depth");
-    std::string file    = getarg("", "-f", "--file");
-
-    if (help || argc <= 1) {
-        show_help();
-    }
-
-    if (version) {
-        std::cout << argv[0] << " demo v1.0.0. Compiled on " << __DATE__ << std::endl;
-    }
-
-    if (depth) {
-        std::cout << "provided depth: " << depth << std::endl;
-    }
-
-    if (!file.empty()) {
-        std::cout << "provided file: " << file << std::endl;
-    }
-
     // OOP map-based api. Explicit (argc, argv) initialization required.
 
     struct getopt args(argc, argv);
 
     if (args.has("-h") || args.has("--help") || args.has("-?") || args.size() == 1) {
         show_help();
+        return EXIT_SUCCESS;
     }
 
     if (args.has("-v") || args.has("--version")) {
         std::cout << args["0"] << " demo v1.0.0. Compiled on " << __DATE__ << std::endl;
+        return EXIT_SUCCESS;
     }
 
     if (args.has("-d") || args.has("--depth") || args.has("--max-depth")) {
@@ -65,4 +44,6 @@ main(int argc, const char **argv)
     std::cout << "---" << std::endl;
     std::cout << args.cmdline() << std::endl;
     //std::cout << args.size() << " provided args: " << args.str() << std::endl;
+
+    return EXIT_SUCCESS;
 }
